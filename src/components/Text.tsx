b127e0110e49c77298e8da43d457b09fd157aa52
Angel Lakra <angel.lakra@chainlabs.in>
2021-10-05 02:36:04 +0530

    text typography update

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import Box, { BoxProps } from 'components/Box';
 
 export const fontSizes = {
-	h1: { mobS: '4.8rem', tabS: '6.4rem', deskM: '7.2rem' },
-	h2: { mobS: '4rem', tabS: '4.8rem', deskM: '6.4rem' },
-	h3: { mobS: '3.6rem', tabS: '4rem', deskM: '4.8rem' },
-	h4: { mobS: '3.2rem', tabS: '3.6rem', deskM: '4rem' },
-	h5: { mobS: '2.4rem', tabS: '3.2rem', deskM: '3.6rem' },
-	h6: { mobS: '2rem', tabS: '2.4rem', deskM: '3.2rem' },
-	b1: { mobS: '1.6rem', tabS: '2rem', deskM: '2.4rem' },
-	b2: { mobS: '1.6rem', tabS: '2rem', deskM: '2.4rem' },
-	b3: { mobS: '1.2rem', tabS: '1.6rem', deskM: '2rem' },
-	links: { mobS: '0.8rem', tabS: '1.2rem', deskM: '1.6rem' },
+	h1: { mobS: '3.2rem', tabS: '3.6rem', deskL: '7.2rem' },
+	h2: { mobS: '2.4rem', tabS: '3.2rem', deskL: '4.8rem' },
+	h3: { mobS: '2rem', tabS: '2.4rem', deskL: '3.6rem' },
+	b1: { mobS: '1.6rem', tabS: '2rem', deskL: '2.4rem' },
+	b2: { mobS: '1.6rem', tabS: '2rem', deskL: '2.4rem' },
+	b3: { mobS: '1.2rem', tabS: '1.6rem', deskL: '2rem' },
+	links: { mobS: '1.4rem', tabS: '1.6rem', deskL: '2rem' },
 };
 
 export const lineHeights = {
-	h1: '150%',
-	h2: '115%',
-	h4: '125%',
+	h1: '115%',
+	h2: '150%',
+	h3: '150%',
+	b1: '150%',
 	b2: '165%',
+	b3: '150%',
 };
 
 const fontWeights = {
