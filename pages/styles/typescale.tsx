ed99ae6d33dd27225531ca72ae4f18076f410993
angel112 <angel.lakra@chainlabs.in>
2022-05-16 21:35:03 +0530

    added typescale

---
+++ pages/styles/typescale.tsx
@@ -0,0 +1,25 @@
+import Box from 'src/components/Box';
+import Text from 'src/components/Text';
+
+const Typescale = () => {
+	return (
+		<Box>
+			<Text as="headline">Headline</Text>
+			<Text as="h1">h1</Text>
+			<Text as="h2">h2</Text>
+			<Text as="h3">h3</Text>
+			<Text as="h4">h4</Text>
+			<Text as="h5">h5</Text>
+			<Text as="h6">h6</Text>
+			<Text as="b1">b1</Text>
+			<Text as="b2">b2</Text>
+			<Text as="b3">b3</Text>
+			<Text as="b4">b4</Text>
+			<Text as="c1">c1</Text>
+			<Text as="c2">c2</Text>
+			<Text as="c3">c3</Text>
+		</Box>
+	);
+};
+
+export default Typescale;
