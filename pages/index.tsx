a6b0b28e76d1afeed168ec16a1451cd18f8faeef
angel112 <angel.lakra@chainlabs.in>
2022-05-16 21:41:53 +0530

    Removed old files
    Signed-off-by: angel112 <angel.lakra@chainlabs.in>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import OuterContainer from 'components/OuterContainer';
-import ChainLabs from 'containers/home';
 
 const Home = () => {
-	return (
-		<OuterContainer bg="purple-black">
-			<ChainLabs />
-		</OuterContainer>
-	);
+	return <OuterContainer bg="purple-black">{/* <ChainLabs /> */}</OuterContainer>;
 };
 
 export default Home;
