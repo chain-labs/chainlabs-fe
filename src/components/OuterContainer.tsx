1399f91135cae608d451f35c8ea48596c8971f79
angel112 <angel.lakra@chainlabs.in>
2022-05-20 03:58:56 +0530

    added home page container

--- src/components/OuterContainer.tsx
+++ src/components/OuterContainer.tsx
@@ -1,8 +1,11 @@
 import Box from 'components/Box';
+import { AnimateSharedLayout, motion } from 'framer-motion';
+import Navbar from './Navbar';
 
-const OuterContainer = ({ children, bg }) => {
+const OuterContainer = ({ children, bg, controls }) => {
 	return (
 		<Box bg={bg} minHeight="100vh">
+			<Navbar controls={controls} />
 			{children}
 		</Box>
 	);
