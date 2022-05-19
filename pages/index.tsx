f558ecaf10ea708ee5392cd2f56f6231bac14ab1
angel112 <angel.lakra@chainlabs.in>
2022-05-20 03:58:04 +0530

    added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import OuterContainer from 'components/OuterContainer';
+import { useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import Box from 'src/components/Box';
+import HomeContainer from 'src/containers/home';
 
 const Home = () => {
-	return <OuterContainer bg="purple-black">{/* <ChainLabs /> */}</OuterContainer>;
+	const controls = useAnimation();
+
+	return (
+		<OuterContainer bg="purple-500" controls={controls}>
+			<HomeContainer control={controls} />
+			<Box height="200vh" bg="purple-500" className="section-2"></Box>
+		</OuterContainer>
+	);
 };
 
 export default Home;
