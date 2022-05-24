82352ef6764b87b5f1206247f1c37b936bf77156
angel112 <angel.lakra@chainlabs.in>
2022-05-24 20:59:52 +0530

    Added a color to bg
    Signed-off-by: angel112 <angel.lakra@chainlabs.in>

--- pages/styles/calendly.tsx
+++ pages/styles/calendly.tsx
@@ -1,5 +1,6 @@
 import { InlineWidget, PopupWidget } from 'react-calendly';
 import Box from 'src/components/Box';
+import theme from 'src/styleguide/theme';
 
 const CALENDLY_URL = 'https://calendly.com/chainlabs/social-media-marketing-intern-15-minutes-call';
 
@@ -7,7 +8,7 @@ const CalendlyPage = () => {
 	if (process.browser) {
 		return (
 			<Box>
-				<InlineWidget url={CALENDLY_URL} />
+				<InlineWidget url={CALENDLY_URL} styles={{ background: theme.colors['purple-500'], height: '100vh' }} />
 			</Box>
 		);
 	} else return <></>;
