a6c44fbeea495cc320276a524f4f42088d9152a9
angel112 <angel.lakra@chainlabs.in>
2022-05-27 03:52:04 +0530

    added image

---
+++ src/components/WorksCard.tsx
@@ -0,0 +1,33 @@
+import Box from './Box';
+import Image from 'next/image';
+import Text from './Text';
+
+export interface WorkCardProps {
+	img: string;
+	title: string;
+	body: string;
+	type: 'NFT Launchpad' | 'NFT Collection' | 'Research';
+	twitter: string;
+	ctaUrl: string;
+}
+
+const WorksCard = ({ img, title, body, type, twitter, ctaUrl }: WorkCardProps) => {
+	return (
+		<Box row>
+			<Box position="relative" height="30rem" width="52rem" mr="wxs">
+				<Image src={img} layout="fill" objectFit="contain" />
+			</Box>
+			<Box>
+				<Text as="h4" color="green-300">
+					{title}
+				</Text>
+				<Text as="b3" color="grey-100">
+					{body}
+				</Text>
+				<Box></Box>
+			</Box>
+		</Box>
+	);
+};
+
+export default WorksCard;
