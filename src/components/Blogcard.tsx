6f64bb9db925b979ce9598b35b6dee129e025dad
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-06-11 01:48:41 +0530

    props seperated

--- src/components/Blogcard.tsx
+++ src/components/Blogcard.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 import Box from './Box';
 import Text from './Text';
 
-const Blogcard = ({ blogdetail }) => {
+const Blogcard = ({ imageUrl, url, title }) => {
 	return (
-		<Box mx="mxxxl" target="_blank" cursor="pointer" as="a" href={blogdetail.link}>
+		<Box mx="mxxxl" target="_blank" cursor="pointer" as="a" href={url}>
 			<Box
 				height={{ mobS: '34.3rem', tabS: '27.6rem', deskM: '22rem' }}
 				width={{ mobS: '20rem', tabS: '16.2rem', deskM: '38rem' }}
 				position="relative"
 				mb="mxl"
 			>
-				<Image src={blogdetail.image} layout="fill" objectFit="cover" />
+				<Image src={imageUrl} layout="fill" objectFit="cover" />
 			</Box>
 			<Box position="absolute">
 				<Text as="h6" color="grey-100" mt="mxxs" width="38rem">
-					{blogdetail.text}
+					{title}
 				</Text>
 			</Box>
 		</Box>
--- src/containers/home/components/BlogCard.tsx
+++ src/containers/home/components/BlogCard.tsx
@@ -4,19 +4,22 @@ import Text from 'src/components/Text';
 import Blogcard from 'src/components/Blogcard';
 const BLOGS = [
 	{
-		image: 'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog1_BP-PR8Z8I.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732691786',
-		text: 'Tokens and Birth of NFTs',
-		link: 'https://blog.chainlabs.in/tokens-and-the-birth-of-nfts-4b02a8ddd98a',
+		imageUrl:
+			'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog1_BP-PR8Z8I.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732691786',
+		title: 'Tokens and Birth of NFTs',
+		blogUrl: 'https://blog.chainlabs.in/tokens-and-the-birth-of-nfts-4b02a8ddd98a',
 	},
 	{
-		image: 'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog2_HWR6OwnWd.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732840482',
-		text: 'Think you know NFTs? Think Again',
-		link: 'https://blog.chainlabs.in/think-you-know-nfts-think-again-ee799f3f4789',
+		imageUrl:
+			'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog2_HWR6OwnWd.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732840482',
+		title: 'Think you know NFTs? Think Again',
+		blogUrl: 'https://blog.chainlabs.in/think-you-know-nfts-think-again-ee799f3f4789',
 	},
 	{
-		image: 'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog3_0ia-0Aet7.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732695004',
-		text: 'You’ll see NFTs in a whole new light read this again',
-		link: 'https://blog.chainlabs.in/youll-see-nfts-in-a-whole-new-light-after-reading-this-d6ebd6aecd9a',
+		imageUrl:
+			'https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Home/Blogs/Desktop_Blog3_0ia-0Aet7.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653732695004',
+		title: 'You’ll see NFTs in a whole new light read this again',
+		blogUrl: 'https://blog.chainlabs.in/youll-see-nfts-in-a-whole-new-light-after-reading-this-d6ebd6aecd9a',
 	},
 ];
 
@@ -32,7 +35,7 @@ const BlogCard = () => {
 				</Box>
 				<Box between flexDirection={{ mobS: 'column', tabS: 'row' }} flexWrap="wrap" py="mxl">
 					{BLOGS.map((blog) => (
-						<Blogcard blogdetail={blog} />
+						<Blogcard imageUrl={blog.imageUrl} url={blog.blogUrl} title={blog.title} />
 					))}
 				</Box>
 				<Box as="a" target="_blank" column alignItems="end" cursor="pointer" href="https://blog.chainlabs.in/">
