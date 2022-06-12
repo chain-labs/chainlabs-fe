75a970e712a4ec41bedd6921a5f2cdc42b8d5555
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-06-12 22:35:33 +0530

    added hover state

--- pages/styles/cards.tsx
+++ pages/styles/cards.tsx
@@ -6,36 +6,50 @@ const MEMBERS = [
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/angel_pfp.png',
 		name: 'Angel Lakra',
 		role: 'front-end lead',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/khushboo_pfp.png',
 		name: 'Khushboo Dalwani',
 		role: 'front-end dev',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/jay_pfp.png',
 		name: 'Jay Kadam',
 		role: 'ui/ux designer',
+		twitterUrl: '@_j4yke_',
+		linkedInUrl: '/in/jaykadam',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/raj_pfp.png',
 		name: 'Raj Paul',
 		role: 'digital designer',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/angel_pfp.png',
 		name: 'Angel Lakra',
 		role: 'front-end lead',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/prabhat_pfp.png',
 		name: 'Prabhat Gupta',
 		role: 'project manager',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 	{
 		image: 'https://ik.imagekit.io/chainlabsfe/pfp/vishakha_pfp.png',
 		name: 'Vishakha Singh',
 		role: 'social media strategist',
+		twitterUrl: '//',
+		linkedInUrl: '/;;',
 	},
 ];
 
@@ -43,7 +57,13 @@ const CardsPage = () => {
 	return (
 		<Box bg="purple-500" row flexWrap="wrap" alignItems="center" minHeight="100vh" mx="auto" px="wl">
 			{MEMBERS.map((member) => (
-				<MemberCard image={member.image} name={member.name} role={member.role} />
+				<MemberCard
+					image={member.image}
+					name={member.name}
+					role={member.role}
+					linkedInUrl={member.linkedInUrl}
+					twitterUrl={member.twitterUrl}
+				/>
 			))}
 		</Box>
 	);
--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -2,16 +2,20 @@ import Box from './Box';
 import Image from 'next/image';
 import theme from 'src/styleguide/theme';
 import Text from './Text';
+import { useState } from 'react';
 
 interface Props {
 	image: string;
 	name: string;
 	role: string;
+	twitterUrl: string;
+	linkedInUrl: string;
 }
 
-const MemberCard = ({ image, name, role }: Props) => {
+const MemberCard = ({ image, name, role, twitterUrl, linkedInUrl }: Props) => {
+	const [opacity, setOpacity] = useState('1');
 	return (
-		<Box mr="7.2rem">
+		<Box mr="7.2rem" cursor="pointer">
 			<Box
 				height={{ mobS: '14.866rem', tabS: '18.7rem', deskM: '24rem' }}
 				width={{ mobS: '14.866rem', tabS: '18.7rem', deskM: '24rem' }}
@@ -22,8 +26,21 @@ const MemberCard = ({ image, name, role }: Props) => {
 					deskM: `${240 / 15}px ${240 / 15}px 0 0 ${theme.colors['green-100']}`,
 				}}
 				mb={{ mobS: 'mxl', tabS: '3.6rem', deskM: 'mxxl' }}
+				cursor="pointer"
+				center
 			>
-				<Image src={image} layout="fill" objectFit="cover" />
+				<Box zIndex={1} column>
+					<Text color="green-100">{twitterUrl}</Text>
+					<Text color="green-100">{linkedInUrl}</Text>
+				</Box>
+				<Box
+					onMouseEnter={() => setOpacity('0.2')}
+					onMouseLeave={() => setOpacity('1')}
+					opacity={opacity}
+					zIndex={1}
+				>
+					<Image src={image} layout="fill" objectFit="cover" />
+				</Box>
 			</Box>
 			<Text as="h5" color="white-10" mb="mxs">
 				{name}
