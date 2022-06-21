5e89d875e8a9f1aa14f4445cc59ab3d8d6c52cb5
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-06-22 00:15:13 +0530

    responsive state modified

--- pages/styles/Testimonial.tsx
+++ pages/styles/Testimonial.tsx
@@ -4,7 +4,14 @@ import TestimonialCard from 'src/components/TestimonialCard';
 
 const Testimonial = () => {
 	return (
-		<Box bg="purple-500" minHeight="100vh" row justifyContent="space-around">
+		<Box
+			bg="purple-500"
+			minHeight="100vh"
+			center
+			flexDirection={{ mobS: 'column', tabL: 'row' }}
+			justifyContent={{ mobS: 'center', tabL: 'space-around' }}
+			// alignItems="center"
+		>
 			<TestimonialCard
 				url="https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Services/Desktop_Primobots_gX1euCNLy.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654882459115"
 				comment="The team at Chain Labs delivered on their promise and beyond. Their smart contract work for Primobots and post-deployment support had a huge positive impact on our NFT project."
