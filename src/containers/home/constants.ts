e8dac2568af230925dc294d13a01fee576ca1d9f
angel112 <angel.lakra@chainlabs.in>
2022-07-05 03:18:29 +0530

    Added work Cards

---
+++ src/containers/home/constants.ts
@@ -0,0 +1,44 @@
+import { WorkCardProps } from 'src/components/WorkCard';
+
+export const WORKS: WorkCardProps[] = [
+	{
+		img: 'https://ik.imagekit.io/chainlabsfe/banners/tr:lo-true/simplr_banner.png',
+		title: 'Simplr Collection',
+		body: [
+			'A simple to use, no-code platform to create NFT smart contracts to help launch your launch your NFT Collection hassle-free.',
+		],
+		type: 'NFT Launchpad',
+		twitter: 'https://twitter.com/SimplrDAO',
+		ctaUrl: 'https://simplrcollection.com/',
+	},
+	{
+		img: 'https://ik.imagekit.io/chainlabsfe/banners/primobots_banner_BQxEcaq_n.png',
+		title: 'Primobots',
+		body: [
+			'5555 Fully 3D Primobots. A story-driven NFT brand.',
+			'Our Role: Smart Contract, Web3 Technical Advisory, Web3 Frontend Integration',
+		],
+		type: 'NFT Collection',
+		twitter: 'https://twitter.com/primobots',
+		ctaUrl: 'https://primobots.io/',
+	},
+	{
+		img: 'https://ik.imagekit.io/chainlabsfe/banners/dob_banner_x2Gc8KtPJ.png',
+		title: 'Daughters of Blockchain',
+		body: [
+			'A female-centric NFT project created by Jillian Hinds-Williams and Lavinia Osbourne.',
+			'Our Role: Smart Contract, Web3 Technical Advisory, Mint Page Development',
+		],
+		type: 'NFT Collection',
+		twitter: 'https://twitter.com/DaughtersOfBC',
+		ctaUrl: 'https://mint.daughtersofblockchain.com/',
+	},
+	{
+		img: 'https://ik.imagekit.io/chainlabsfe/banners/ipvc_banner_sdlc4ZGET.png',
+		title: 'IPVC',
+		body: [
+			'A research that shows how tokenizing vaccination certificates can help solve the issues faced by already existing medical systems when it comes to cross-border medical data verification.',
+		],
+		type: 'Research Project',
+	},
+];
