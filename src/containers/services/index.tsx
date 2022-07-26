cecbace40d541da92395ae4dbe8c4c658f97358c
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-07-27 02:17:47 +0530

    Update index.tsx

--- src/containers/services/index.tsx
+++ src/containers/services/index.tsx
@@ -82,20 +82,20 @@ const Services = () => {
 					<TestimonialCard
 						url="https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Services/Desktop_Primobots_gX1euCNLy.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654882459115"
 						comment="The team at Chain Labs delivered on their promise and beyond. Their smart contract work for Primobots and post-deployment support had a huge positive impact on our NFT project."
-						name="Ish"
-						title="Founder, Primobots"
-					/>
-					<TestimonialCard
-						url="https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Services/Desktop_DOB_TSwibgD0m.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654882459115"
-						comment="Chain Labs went the extra mile to build, launch and support the Daughters of Blockchain Genesis collection of 1,000 unique NFTs minting on Polygon."
 						name="Baps"
-						title="Founder, Daughters of Blockchain"
+						title="Founder, Primobots"
 					/>
 					<TestimonialCard
 						url="https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Services/Desktop_DOB_TSwibgD0m.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654882459115"
 						comment="Chain Labs went the extra mile to build, launch and support the Daughters of Blockchain Genesis collection of 1,000 unique NFTs minting on Polygon."
 						name="Jillian"
 						title="Founder, Daughters of Blockchain"
+					/>
+					<TestimonialCard
+						url="https://ik.imagekit.io/chainlabs/Website_Media/Desktop/Services/Desktop_DOB_TSwibgD0m.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654882459115"
+						comment="Chain Labs went the extra mile to build, launch and support the Daughters of Blockchain Genesis collection of 1,000 unique NFTs minting on Polygon."
+						name="Jillian"
+						title="Founder, Block Ape Lads"
 					/>
 				</Box>
 			</Box>
