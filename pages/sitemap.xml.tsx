e6f5f68478dd72d860392ddecbe845dbe0b329ca
angel112 <angel.lakra@chainlabs.in>
2022-08-01 20:53:35 +0530

    update sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,16 +3,33 @@ const Sitemap = () => {
 };
 
 export const getServerSideProps = async ({ res }) => {
-	const BASE_URL = 'http://chainlabs.in/';
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-        <loc>${BASE_URL}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-        </url>
-        </urlset>
+  <urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+     <url>
+        <loc>http://www.chainlabs.in/</loc>
+        <lastmod>2022-07-30</lastmod>
+     </url>
+     <url>
+        <loc>http://www.chainlabs.in/contact</loc>
+        <lastmod>2022-07-30</lastmod>
+     </url>
+     <url>
+        <loc>http://www.chainlabs.in/about</loc>
+        <lastmod>2022-07-30</lastmod>
+     </url>
+     <url>
+        <loc>http://www.chainlabs.in/services</loc>
+        <lastmod>2022-07-30</lastmod>
+     </url>
+     <url>
+        <loc>https://www.chainlabs.in/contact/calendly</loc>
+        <lastmod>2022-07-30</lastmod>
+     </url>
+  </urlset>
   `;
 
 	res.setHeader('Content-Type', 'text/xml');
